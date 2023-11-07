80b10eb82efe353753986537c21ce7b77e6ee3a1
Steven Dew <stevendew02@gmail.com>
2023-11-07 13:50:06 -0600

    📦  FEAT: wippy

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,33 @@
 package main
 
 import (
-    // "database/sql"
-    // "fmt"
-    "log"
-    // "net/http"
+	// "database/sql"
+	// "fmt"
+	"log"
+	// "net/http"
 
-    _ "github.com/lib/pq"
-    "github.com/stevend2002/tgp-bp/internal/db"
-    "github.com/stevend2002/tgp-bp/internal/repository"
-    "github.com/stevend2002/tgp-bp/internal/service"
-    // "github.com/stevend2002/tgp-bp/pkg/errors"
-    "github.com/stevend2002/tgp-bp/pkg/httpserver"
+	_ "github.com/lib/pq"
+	"github.com/stevend2002/tgp-bp/internal/db"
+	"github.com/stevend2002/tgp-bp/internal/repository"
+	"github.com/stevend2002/tgp-bp/internal/service"
+
+	// "github.com/stevend2002/tgp-bp/pkg/errors"
+	"github.com/stevend2002/tgp-bp/pkg/httpserver"
 )
-
 
 const (
-    dbConnectionString = "host=localhost port=5432 dbname=postgres user=myuser sslmode=disable"
+	dbConnectionString = "host=localhost port=5432 dbname=postgres user=myuser sslmode=disable"
 )
 
-
 func main() {
-    err := db.InitDB(dbConnectionString)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err := db.InitDB(dbConnectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    userRepository := repository.NewUserRepository(db.DB)
-    userService := service.NewUserService(userRepository)
+	userRepository := repository.NewUserRepository(db.DB)
+	userService := service.NewUserService(userRepository)
 
-    httpServer := httpserver.NewHTTPServer(userService)
-    httpServer.StartServer()
+	httpServer := httpserver.NewHTTPServer(userService)
+	httpServer.StartServer()
 }
-
