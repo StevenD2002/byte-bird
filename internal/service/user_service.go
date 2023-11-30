5d56d66c479ddefb6096bdd5b1f580dd832cf875
Steven Dew <stevendew02@gmail.com>
2023-11-29 22:55:54 -0600

    📦  FEAT: add login route

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,31 +1,81 @@
 package service
 
 import (
-  "golang.org/x/crypto/bcrypt"
-
 	"byte-bird/internal/repository"
+	"context"
+	"fmt"
+	"time"
+  "strconv"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService interface {
 	CreateUser(name string, email string, password string) error
+	AuthenticateUser(ctx context.Context, email string, password string) (string, error)
 }
 
 type userService struct {
 	userRepository repository.UserRepository
 }
 
+type Claims struct {
+  UserID int `json:"user_id"`
+  Email string `json:"email"`
+  jwt.StandardClaims
+}
+
+
 func NewUserService(userRepository repository.UserRepository) UserService {
 	return &userService{userRepository}
 }
 
 func (us *userService) CreateUser(name string, email string, password string) error {
-  hashedPassword := hashedPassword(password)
+	hashedPassword := hashPassword(password)
 
 	return us.userRepository.CreateUser(name, email, hashedPassword)
 }
 
+func (us *userService) AuthenticateUser(ctx context.Context, email string, password string) (string, error) {
+	userId, userPassword, userEmail, err := us.userRepository.GetUserByEmail(ctx, email)
 
-func hashedPassword(password string) string {
-  bytes, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
-  return string(bytes)
+  //parse the userId into an int
+  userIdInt, err := strconv.Atoi(userId)
+	if err != nil {
+		return "", err
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(userPassword), []byte(password))
+	if err != nil {
+		return "", fmt.Errorf("invalid credentials")
+	}
+
+	// generate the token
+	token, err := generateToken(userIdInt, userEmail)
+	if err != nil {
+		return "", fmt.Errorf("error generating token")
+	}
+
+	return token, nil
+}
+
+func hashPassword(password string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes)
+}
+
+func generateToken(userID int, email string) (string, error) {
+	claims := Claims{
+		UserID: userID,
+		Email:  email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+      IssuedAt: time.Now().Unix(),
+		},
+	}
+
+  token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+  return token.SignedString([]byte("temp-secret-key"))
+
 }
--- pkg/httpserver/server.go
+++ pkg/httpserver/server.go
@@ -2,6 +2,7 @@ package httpserver
 
 import (
 	"byte-bird/internal/service"
+	"context"
 	"encoding/json"
 	"net/http"
 )
@@ -16,6 +17,7 @@ func NewHTTPServer(userService service.UserService) HTTPServer {
 
 func (s HTTPServer) StartServer() {
 	http.HandleFunc("/register", s.handleRegisterUser)
+	http.HandleFunc("/login", s.handleLoginUser)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -37,9 +39,8 @@ func (s HTTPServer) handleRegisterUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-
 	// Start the create user process
-  err := s.userService.CreateUser(user.Name, user.Email, user.Password)
+	err := s.userService.CreateUser(user.Name, user.Email, user.Password)
 	if err != nil {
 		http.Error(w, "Error creating user", http.StatusInternalServerError)
 		return
@@ -48,3 +49,42 @@ func (s HTTPServer) handleRegisterUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
+func (s HTTPServer) handleLoginUser(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	var user struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&user); err != nil {
+		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+		return
+	}
+	// Start the login user process
+	token, err := s.userService.AuthenticateUser(ctx, user.Email, user.Password)
+	if err != nil {
+		http.Error(w, "Error logging in user", http.StatusInternalServerError)
+		return
+	}
+	// build the response object
+
+	response := map[string]interface{}{
+		"status": "success",
+		"token":  token,
+	}
+
+  jsonRespone, err := json.Marshal(response)
+  if err != nil {
+    http.Error(w, "Error marshalling response", http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(http.StatusOK)
+  w.Write(jsonRespone)
+}
